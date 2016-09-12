807eb1881f5931203558faaa7552d9fca355a339
Vasilis Kortsimelidis <vaskort@gmail.com>
2016-09-12 22:56:45 +0100

    8th video udemy

--- public/app.jsx
+++ public/app.jsx
@@ -1,14 +1,38 @@
 var Greeter = React.createClass({
+  getDefaultProps: function(){
+    return {
+      name: 'React',
+      message: 'Message from the component'
+    }
+  },
+  onButtonClick: function(e) {
+    e.preventDefault();
+
+    var name = this.props.name.value;
+
+    alert(name);
+
+  },
   render: function(){
+    var name = this.props.name;
+    var message = this.props.message;
     return (
       <div>
-        <h1>Hello React!</h1>
+        <h1>Hello {name}!</h1>
+        <p>{message}</p>
+
+        <form onSubmit={this.onButtonClick}>
+          <input type="text" value="name" />
+          <button>Set name </button>
+        </form>
       </div>
     );
   }
 });
 
+var firstName = 'Vasilis';
+
 ReactDOM.render(
-  ,
+  <Greeter firstName="Vasilis" message="message from the render" />,
   document.getElementById('app')
 );
