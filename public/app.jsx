dbef28691a31407383ab3e3a00c6aaac46deaabb
Vasilis Kortsimelidis <vaskort@gmail.com>
2016-09-12 18:17:39 +0100

    udemy commit

---
+++ public/app.jsx
@@ -0,0 +1,14 @@
+var Greeter = React.createClass({
+  render: function(){
+    return (
+      <div>
+        <h1>Hello React!</h1>
+      </div>
+    );
+  }
+});
+
+ReactDOM.render(
+  ,
+  document.getElementById('app')
+);
