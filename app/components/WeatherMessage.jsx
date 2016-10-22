7e9049a875618b810a6c2d7fc83d5fc172d8218a
Vasilis Kortsimelidis <vaskort@gmail.com>
2016-10-22 17:49:56 +0100

    end of section 5

--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -1,15 +1,9 @@
 var React = require('react');
 
-var WeatherMessage = React.createClass({
-
-  render: function() {
-    var {temp, location} = this.props;
-
-    return (
-      <p>It is {temp} in {location}</p>
-    );
-  }
-
-});
+var WeatherMessage = ({temp, location}) => {
+  return (
+    <p>It is {temp} in {location}</p>
+  );
+}
 
 module.exports = WeatherMessage;
