7cebd74d74651c645db5ed1bae4aa33ad56db31a
Vasilis Kortsimelidis <vaskort@gmail.com>
2016-11-06 01:30:00 +0000

    latest

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 
 // Create our app
 var app = express();
-const port = process.env.port || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(function (req, res, next){
   if (req.headers['x-forwarded-proto'] === 'http') {
@@ -14,6 +14,6 @@ app.use(function (req, res, next){
 
 app.use(express.static('public'));
 
-app.listen(port, function () {
+app.listen(PORT, function () {
   console.log('Express server is up on port ' + PORT);
 });
