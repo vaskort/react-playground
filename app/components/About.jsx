33323aa81a7cbdb07977e161173b5fe12272b367
Vasilis Kortsimelidis <vaskort@gmail.com>
2016-11-06 18:41:03 +0000

    Style About page & link to github

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,8 +3,10 @@ var React = require('react');
 var About = (props) => {
   return (
     <div>
-      <h3>About component</h3>
+      <h1 className="text-center">About component</h1>
       <p>Welcome to the about page</p>
+      <p>This is a mini-project to learn ReactJS. You can search for a city to check its weather</p>
+      <p>Check the repo on <a href="https://github.com/vaskort/react-playground" target="_blank">GitHub</a></p>
     </div>
   );
 }
