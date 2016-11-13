d96d0b3efbeda07c8a00eeee04601f6818a7f035
Vasilis Kortsimelidis <vaskort@gmail.com>
2016-11-13 18:09:24 +0000

    Add error modal

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -49,7 +49,7 @@ var Weather = React.createClass({
     function renderError() {
       if (typeof errorMessage === 'string') {
         return (
-          <ErrorModal />
+          <ErrorModal message={ errorMessage }/>
         )
       }
     }
